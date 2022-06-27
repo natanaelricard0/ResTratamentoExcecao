df78128f6b9bde4aee0be8ea0a75c759f0b0b204
Natanael Ricardo <nathanshurle@gmail.com>
2022-06-27 11:25:31 -0300

    classes finalizadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,44 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import models.entities.Account;
+import models.exceptions.BusinessException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Infome os dados da sua conta: ");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		} catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
 
 	}
 
